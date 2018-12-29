d8cd381dd1be80c103889bc0efc0ef2827d48a15
Dasarathy Raghavan <draghavan@dasarathysmbp2.attlocal.net>
2018-12-29 17:53:05 -0500

    notes app initial commit with yarn

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('Starting app.js');
+
+const fs = require ('fs');
+const os = require('os');
+
+const notes = require('./notes.js')
+
+console.log(notes.addNote());
+
+console.log(notes.add(9, -2));
+
+// const userInfo = os.userInfo();
+// let fileName = 'greetings.txt'
+
+// console.log(userInfo.username);
+
+// fs.appendFile(fileName, `Hello ${userInfo.username}`, (err) => {
+//   if (err) {
+//     console.log('Unable to append to file');
+//   } else {
+//     console.log(`"Hello ${userInfo.username}" written to ${fileName}`)
+//   }
+// })
